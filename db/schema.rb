345acc0a67249a03fff894954690fc0a5c5c7266
Erik Froese <froesecom@gmail.com>
2014-05-19 16:47:13 +1000

    changed to postres database

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140515115209) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "categories", force: true do |t|
     t.text     "name"
     t.datetime "created_at"
